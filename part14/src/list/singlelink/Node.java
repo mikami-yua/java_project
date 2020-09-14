1e0001a0263f6fb423c007ca4a5100aa71d5a4e1
mikami-yua <lovejiaxi_1@163.com>
2020-09-14 18:04:51 +0800

    creat project

--- part14/src/list/singlelink/Node.java
+++ part14/src/list/singlelink/Node.java
@@ -11,4 +11,14 @@ public class Node {
     Object element;//存储的数据
     Node next;//下一个节点的内存地址
     //测试
+    public Node(){
+
+    }
+
+    public Node(Object element,Node next){
+        this.element=element;
+        this.next=next;
+    }
+
+
 }
